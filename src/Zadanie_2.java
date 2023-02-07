b1dd0f28b143d6817ae3c8fb3d1e4c91fc7f6602
Левочкин Борис Владимирович (4062552) <blevochkin@vtb.ru>
2023-02-08 02:22:58 +0300

    2

--- src/Zadanie_2.java
+++ src/Zadanie_2.java
@@ -1,6 +1,13 @@
 public class Zadanie_2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("Минимальное значение = ");
+       printThreeWords();
+
+    }
+    public static void printThreeWords(){
+
+        System.out.println("Orange\nBanana\nApple"  );
+        return;
+
     }
 }
