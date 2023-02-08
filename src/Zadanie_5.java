2ef14ecaefa9eebee9e553fee244d59623a779a2
Левочкин Борис Владимирович (4062552) <blevochkin@vtb.ru>
2023-02-08 03:18:00 +0300

    5

---
+++ src/Zadanie_5.java
@@ -0,0 +1,21 @@
+public class Zadanie_5 {
+    public static void main(String[] args) {
+
+        compareNumbers();
+
+    }
+    public static void compareNumbers(){
+        int a = 1;
+        int b = 2;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+
+        return;
+    }
+
+}
