afe45e8b3869058dd20d7b78b780285e28e6323b
Левочкин Борис Владимирович (4062552) <blevochkin@vtb.ru>
2023-02-08 03:23:09 +0300

    6

---
+++ src/Zadanie_6.java
@@ -0,0 +1,15 @@
+public class Zadanie_6 {
+    public static void main(String[] args) {
+        Zadanie_2.printThreeWords();
+        Zadanie_3.checkSumSign();
+        Zadanie_4.printColor();
+        Zadanie_5.compareNumbers();
+    }
+
+
+
+
+
+
+
+}
