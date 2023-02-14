e414d63345b19e356fd89cae8d9f08869d2c2119
Левочкин Борис Владимирович (4062552) <blevochkin@vtb.ru>
2023-02-14 16:39:20 +0300

    1

--- src/Urok_2/Zadanie1.java
+++ src/Urok_2/Zadanie1.java
@@ -1,4 +1,25 @@
 package Urok_2;
 
 public class Zadanie1 {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+
+        System.out.println(checkSum());
+
+    }
+    public static void checkSum(int a, int b){
+
+        if ((a + b >= 10) && (a + b <= 20))  return true;
+
+        else return true;
+
+
+
+
+
+
+
 }
